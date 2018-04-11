60430f78ad0b4efbb831a89034d9ccd4f6ffb457
fujita.y <fujita.y@edocode.co.jp>
2018-04-11 15:42:35 +0900

    add initial message

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 (() => {
     let counter = 0;
     const app = document.querySelector('#app');
+    const interval = 10000;
+
+    app.textContent = `Interval: ${interval / 1000} seconds`;
 
     function printLog(kind, message) {
         app.textContent = `[${new Date()}][${kind}] ${message}\n${app.textContent}`
@@ -10,6 +13,6 @@
         navigator.geolocation.getCurrentPosition(pos => printLog('location', `緯度: ${pos.coords.latitude}, 経度: ${pos.coords.longitude}`));
         printLog('countup', `counter: ${counter}`);
         counter++;
-    }, 10000);
+    }, interval);
 
 })();
