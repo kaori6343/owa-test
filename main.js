9c5698fcb0ba5125b43d0f09d07493d582b9103c
fujita.y <fujita.y@edocode.co.jp>
2018-04-11 15:00:16 +0900

    Add location logging

--- main.js
+++ main.js
@@ -2,8 +2,14 @@
     let counter = 0;
     const app = document.querySelector('#app');
 
+    function printLog(kind, message) {
+        app.textContent = `[${new Date()}][${kind}] ${message}\n${app.textContent}`
+    }
+
     setInterval(() => {
-        app.textContent = `[${new Date()}] counter: ${counter}\n${app.textContent}`;
+        navigator.geolocation.getCurrentPosition(pos => printLog('location', `緯度: ${pos.coords.latitude}, 経度: ${pos.coords.longitude}`));
+        printLog('countup', `counter: ${counter}`);
         counter++;
     }, 1000);
+
 })();
