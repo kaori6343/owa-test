2e7db686871c4aa2fd556d964802e4523072fb7a
fujita.y <fujita.y@edocode.co.jp>
2018-04-11 14:24:11 +0900

    Update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+(() => {
+    let counter = 0;
+    const app = document.querySelector('#app');
 
+    setInterval(() => {
+        app.textContent += `[${new Date()}] counter: ${counter}`;
+        counter++;
+    }, 1000);
+})();
